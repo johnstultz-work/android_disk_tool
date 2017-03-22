edfe57fb55ce2895c040637c94c736aa97f0e575
John Stultz <john.stultz@linaro.org>
2017-03-22 09:42:43 -0700

    test-deploy.sh: Add initial notes for spining up a CTS test runner
    All commented out, but adds some initial notes on steps
    needed to spin up a CTS test runner more automatically.
    
    Still need to figure out how to automatically build and
    deploy a docker image to an instance as well as how
    to automatically connect to specific device instances
    and run the tests
    
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- test-deploy.sh
+++ test-deploy.sh
@@ -38,4 +38,19 @@ gcloud compute images create "$IMGNAME" \
 	--source-uri "https://storage.googleapis.com/$BUCKET/$DISKIMG"
 gcloud compute instances create "android-$NOW" --machine-type "custom-1-1024" --image "$IMGNAME"
 
+# Spawn and setup CTS test runner
+# gcloud compute instances create "cts-test-runner-$NOW" --machine-type "n1-standard-1" --image "cos-stable-56-9000-84-2" --image-project "cos-cloud" --boot-disk-size "20"
+#
+#### <ssh to cts-test-runner> ###
+# $ docker build -t cts https://raw.githubusercontent.com/johnstultz-work/dockerstuff/master/Dockerfile.cts
+# $ docker run -t -i cts
+#
+#### now in running docker image ####
+# $ cd
+# $ unzip android-cts-7.1_r1-linux_x86-x86.zip
+# $ cd android-cts/tools
+# $ adb connect <ip for android instance>
+# $ ./cts-tradefed
+# cts-tf > run cts
+
 
